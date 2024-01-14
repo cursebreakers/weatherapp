46e2d6e3627be210fd0905a69db2afc47c278c9f
Esau <cursebreakers.usa@gmail.com>
2024-01-14 17:17:12 -0500

    working zip search ii https fix

--- src/call.js
+++ src/call.js
@@ -9,7 +9,7 @@ const weatherData = {
 
 export async function zipWeather(zipCode) {
     try {
-      const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=6fd9b2d8ba534774b56192624241401&q=${zipCode}&aqi=no`);
+      const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=6fd9b2d8ba534774b56192624241401&q=${zipCode}&aqi=no`);
       const data = await response.json();
   
       // Update weatherData with API response
